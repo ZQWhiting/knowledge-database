4150bd4b348f604267df0508b0dc799a1e439441
ZQWhiting <zach.whiting@icloud.com>
2021-07-30 15:48:32 -0600

    CreateTag styling

---
+++ client/src/components/CreateTag/index.js
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { CREATE_TAG } from '../../utils/mutations';
+import classNames from 'classnames';
+import './style.scss';
+
+function CreateTag() {
+	const [active, setActive] = useState(false);
+	const [inputError, setInputError] = useState(false);
+	const [name, setName] = useState('');
+	const [createTag] = useMutation(CREATE_TAG, {
+		onCompleted: () => {
+			console.log('Tag successfully saved.');
+			setName('');
+			setActive(false);
+		},
+		onError: (e) => {
+			setInputError(true);
+			console.error(e.message);
+		},
+		variables: { name: name.trim() },
+	});
+
+	const submitInput = () => {
+		if (name) createTag();
+		else setActive(false);
+	};
+
+	const activateForm = () => {
+		setActive(true);
+	};
+
+	return active ? (
+		<input
+			onBlur={submitInput}
+			type='text'
+			name='name'
+			id='tag-name'
+			onChange={(e) => setName(e.target.value)}
+			value={name}
+			placeholder='new tag'
+			onFocus={() => setInputError(false)}
+			autoFocus
+			className={classNames('input', {
+				error: inputError ? true : false,
+			})}
+		></input>
+	) : (
+		<FontAwesomeIcon
+			icon={faPlusCircle}
+			className={classNames('add-tag-btn')}
+			onClick={activateForm}
+		/>
+	);
+}
+
+export default CreateTag;
